98caeb349c64a9143a6a61bfecd68739a61f28cd
Amadeu Marques <maramadeu@gmail.com>
2025-09-21 20:41:04 +0100

    increase chunks

--- src/lib.rs
+++ src/lib.rs
@@ -240,7 +240,7 @@ impl DivisibleState for StateOrchestrator {
         // println!("prefix count {:?}", self.updates.seqno);
         println!("updates {:?}", self.updates.len());
 
-        let chunks = split_evenly(&self.updates.extract(), 36)
+        let chunks = split_evenly(&self.updates.extract(), 48)
             .map(|chunk| chunk.to_owned())
             .collect::<Vec<_>>();
 
